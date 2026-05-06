46116551484ae57d2d83442a55957551f0411343
Dries Gouwy <dries.gouwy@devoteam.com>
2026-05-06 13:26:47 +0000

    commit last bit about sources

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod,
-    status,
+    status as order_status,
     amount,
     created
 from {{ source('stripe', 'payment') }}
